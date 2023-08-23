d4385fe84a3127ead4c58b3d1a629b3218d02dbc
danielbilsborrow <100538713+danielbilsborrow@users.noreply.github.com>
2023-08-23 16:59:00 +0100

    changing to correct download of LH408

--- downloadLHsim.py
+++ downloadLHsim.py
@@ -12,31 +12,31 @@ def download_file(url, save_path):
     else:
         print(f"Failed to download: {url}", flush=True)
 
-
+filenum = 408
 if __name__ == "__main__":
 
-    for filenum in np.arange(0,1000):
+#    for filenum in np.arange(0,1000):
 #        filenum = str("{:03d}".format(filenum))
-        url = f"https://users.flatironinstitute.org/~camels/FOF_Subfind/IllustrisTNG/LH/LH_{filenum}/fof_subhalo_tab_033.hdf5"
-        save_directory = "../CAMELS/."
-        new_file_name = f"LH{filenum}_fof_subhalo_tab_033.hdf5"  # Change this to the new desired name
-        save_path = os.path.join(save_directory, new_file_name)
+    url = f"https://users.flatironinstitute.org/~camels/FOF_Subfind/IllustrisTNG/LH/LH_{filenum}/fof_subhalo_tab_033.hdf5"
+    save_directory = "../CAMELS/."
+    new_file_name = f"LH{filenum}_fof_subhalo_tab_033.hdf5"  # Change this to the new desired name
+    save_path = os.path.join(save_directory, new_file_name)
 
-        if not os.path.exists(save_directory):
-            os.makedirs(save_directory)
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
 
-        download_file(url, save_path)
+    download_file(url, save_path)
 
-        url = f"https://users.flatironinstitute.org/~camels/Sims/IllustrisTNG/LH/LH_{filenum}/snap_033.hdf5"
-        new_file_name = f"LH{filenum}_snap_033IllustrisTNG.hdf5"  # Change this to the new desired name
-        save_path = os.path.join(save_directory, new_file_name)
+    url = f"https://users.flatironinstitute.org/~camels/Sims/IllustrisTNG/LH/LH_{filenum}/snap_033.hdf5"
+    new_file_name = f"LH{filenum}_snap_033IllustrisTNG.hdf5"  # Change this to the new desired name
+    save_path = os.path.join(save_directory, new_file_name)
 
-        if not os.path.exists(save_directory):
-            os.makedirs(save_directory)
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
 
-        download_file(url, save_path)
+    download_file(url, save_path)
 
-        print("done")
+    print("done")
 
 
 
