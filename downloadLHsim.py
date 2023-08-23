01efefb8a757a0b755f6b6c421abc8394847f2c5
danielbilsborrow <100538713+danielbilsborrow@users.noreply.github.com>
2023-08-23 20:10:33 +0100

    download LH

--- downloadLHsim.py
+++ downloadLHsim.py
@@ -12,7 +12,7 @@ def download_file(url, save_path):
     else:
         print(f"Failed to download: {url}", flush=True)
 
-filenum = 408
+filenum = 599
 if __name__ == "__main__":
 
 #    for filenum in np.arange(0,1000):
