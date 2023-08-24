9f5dca0b2930e0e390f37d0ba0589f48b4132d68
danielbilsborrow <100538713+danielbilsborrow@users.noreply.github.com>
2023-08-25 00:32:18 +0100

    try again

--- combining_h5.py
+++ combining_h5.py
@@ -12,14 +12,17 @@ for i in range(18):
 # Output HDF5 file (the combined file)
 output_file = '../CAMELS/correlation_funcs/all_IllustrisLH_corfuncs.h5'
 
-# Create the output HDF5 file
-with h5py.File(output_file, 'w') as out_f:
+# Open the output file in write mode
+with h5py.File(output_file, 'w') as out_file:
     for input_file in input_files:
-        with h5py.File(input_file, 'r') as in_f:
-            for dataset_name in in_f:
-                in_dataset = in_f[dataset_name]
-                # Copy the dataset from the input file to the output file
-                out_dataset = out_f.create_dataset(dataset_name, data=in_dataset)
+        # Open each input file in read mode
+        with h5py.File(input_file, 'r') as in_file:
+            # Iterate through datasets in the input file
+            for dataset_name in in_file:
+                # Create a group in the output file for each dataset
+                group = out_file.create_group(dataset_name)
+                # Copy data from the input file to the output file
+                in_file[dataset_name].copy(group)
 
 print(f"Combined HDF5 files into '{output_file}'.")
 
