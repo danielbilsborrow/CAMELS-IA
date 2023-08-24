2894dfcfefac54af85d3a494475a763793651902
danielbilsborrow <100538713+danielbilsborrow@users.noreply.github.com>
2023-08-24 12:47:56 +0100

    550-600

--- 9b.findNGcorr_sliced.py
+++ 9b.findNGcorr_sliced.py
@@ -64,7 +64,7 @@ def slicer(array, num_slices=15, ell_dict=None, ell=False):
 
 ####################
 #filenum = 12
-for filenum in range(500,550):
+for filenum in range(550,600):
     # Load dictionaries from the pickled file
     with open(f'../CAMELS/ellipticity_measurements/LH{filenum}_ellipticities.pkl', 'rb') as f:
         data = pickle.load(f)
@@ -114,7 +114,7 @@ for filenum in range(500,550):
     ############################
 
     # Create an HDF5 file (or open if it exists)
-    with h5py.File('../CAMELS/correlation_funcs/IllustrisLH500-550_corfuncs.h5', 'a') as hf:
+    with h5py.File('../CAMELS/correlation_funcs/IllustrisLH550-600_corfuncs.h5', 'a') as hf:
         # Create datasets for simulation 1 arrays
         hf.create_dataset(f'simulation{filenum}/corrfunc', data=aggregate_corr_ng)
         hf.create_dataset(f'simulation{filenum}/corrvar', data=aggregate_corr_ngvar)
